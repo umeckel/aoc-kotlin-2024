f240471b81dfee5c25aafeab5c73c62f2a4a64fc
umeckel <umeckel@gmail.com>
2024-12-01 12:01:16 +0100

    Testing AI Assistant: Refactor Day01 by adding new functionalities
    The main function in Day01.kt was updated to add new functionalities. Two new helper functions: `splitAndSortLists` and `calculateDifferences` were introduced to dissect string lists and calculate differences between two lists of integers, respectively.
    
    The existing `part1` and `part2` functions were also refactored to integrate these new helper functions and now return the sum of the calculated differences and the sum of the values after multiplication with occurrence counts respectively.
    
    Two new test checks were added to verify these new implementations.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,68 @@
 fun main() {
+    fun splitAndSortLists(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+
+        input.forEach { item ->
+            val parts = item.split(" ")
+            val left = parts.first().toInt()
+            val right = parts.last().toInt()
+            leftList.add(left)
+            rightList.add(right)
+//            if (left !in leftList) leftList.add(left)
+//            if (right !in rightList) rightList.add(right)
+
+        }
+
+        leftList.sort()
+        rightList.sort()
+        return Pair(leftList, rightList)
+    }
+
+    fun calculateDifferences(leftList: MutableList<Int>, rightList: MutableList<Int>): MutableList<Int> {
+        val differences = mutableListOf<Int>()
+
+        for (idx in 0 until leftList.size) {
+            differences.add(kotlin.math.abs(leftList[idx] - rightList[idx]))
+        }
+
+        return differences
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val (leftList, rightList) = splitAndSortLists(input)
+
+        val diffs = calculateDifferences(leftList, rightList)
+
+        return diffs.sum()
+    }
+
+    fun multiplyWithOccurence(leftList: MutableList<Int>, rightList: MutableList<Int>): MutableList<Int> {
+        val multiplied = mutableListOf<Int>()
+        val occurrences = rightList.groupingBy { it }.eachCount()
+
+        leftList.forEach { 
+            multiplied.add(it * (occurrences[it] ?: 0))
+        }
+        return multiplied
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (leftList, rightList) = splitAndSortLists(input)
+
+        val multiplied = multiplyWithOccurence(leftList, rightList)
+
+        return multiplied.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     val input = readInput("Day01")
     part1(input).println()
+
+    check(part2(testInput) == 31)
     part2(input).println()
 }
